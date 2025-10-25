87e0c3cd4f01f72350b7935d1c7e59ca9dd750ed
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-10-25 14:06:41 -0500

    Create tts.js

---
+++ utils/tts.js
@@ -0,0 +1,4 @@
+export async function textToSpeech(text) {
+  // Genera un audio usando la Web Speech API en frontend
+  return `https://dummyimage.com/600x100/000/fff&text=${encodeURIComponent(text)}`;
+}
